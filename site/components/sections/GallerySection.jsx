8d3b2b28707ac1e5e8f024fd56e6764db089ed00
Austin Porada <austin@theporadas.com>
2025-10-07 23:05:55 -0400

    chore(lint): remove final leftover eslint-disable directive

--- site/components/sections/GallerySection.jsx
+++ site/components/sections/GallerySection.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-
+              
 import GalleryDisplay from '../GalleryDisplay';
 import SectionTransition from '../SectionTransition';
 import VideoPlayer from '../VideoPlayer';
